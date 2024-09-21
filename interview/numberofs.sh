40d2f68789dc9d9b1fe2ed5c62c1b516cd19f5bf
Shravan <shravanchauhan0055@gmail.com>
2024-09-22 02:33:13 +0530

    22

--- interview/numberofs.sh
+++ interview/numberofs.sh
@@ -14,5 +14,10 @@ grep -o "m" <<<"$y" | wc -l  # -o=only, wc -l word count command.
 
 grep -o "u" <<<"$y" | wc -l
 
+grep -o -i "devops" *.html | wc -l
 
+grep -o -i "devops" *.html: Searches for the word "devops" (case-insensitive due to
+-i) in all HTML files and outputs each occurrence on a new line (-o).
+wc -l: Counts the number of lines output by grep, which corresponds to the number of
+occurrences of "devops"
 
